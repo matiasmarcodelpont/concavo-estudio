a9c258465bf65b847fb7b75bd32b36bdee8c5b0f
Matías Marcó del Pont <48724650+matiasmarcodelpont@users.noreply.github.com>
2024-04-29 16:11:55 -0300

    Handle 404 on room page (#12)

--- src/app/casa/[roomSlug]/page.tsx
+++ src/app/casa/[roomSlug]/page.tsx
@@ -1,16 +1,21 @@
 import Link from 'next/link'
+import { notFound } from 'next/navigation'
 
 import { contributorsRepository, productsRepository, roomsRepository } from '@/controllers'
 
 export default function Room({ params }: { params: { roomSlug: string } }) {
-  const room = roomsRepository.getRoom(params.roomSlug)
-  const products = productsRepository.getProductsInRoom(params.roomSlug)
-  const mainContributors = contributorsRepository.getMainContributorsByRoom(params.roomSlug)
-  const standardContributors = contributorsRepository.getStandardContributorsByRoom(params.roomSlug)
+  const room = roomsRepository.getRoom(decodeURIComponent(params.roomSlug))
+  if (!room) {
+    notFound()
+  }
+
+  const products = productsRepository.getProductsInRoom(room.slug)
+  const mainContributors = contributorsRepository.getMainContributorsByRoom(room.slug)
+  const standardContributors = contributorsRepository.getStandardContributorsByRoom(room.slug)
 
   return (
     <main>
-      <h1>{room?.name}</h1>
+      <h1>{room.name}</h1>
       <section>
         <ul aria-label='Lista de Productos'>
           {products.map((product) => (
