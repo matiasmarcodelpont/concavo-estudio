f9c8f3c5cd34ba82745a53797b36aafd18531344
Matías Marcó del Pont <matiasmdelp@gmail.com>
2024-05-08 17:35:27 -0300

    Simplify colaborador component html & css

--- src/components/domain/colaborador.tsx
+++ src/components/domain/colaborador.tsx
@@ -13,15 +13,15 @@ interface ColaboradorBaseProps {
 type StandardColaboradorProps = ColaboradorBaseProps &
   Omit<StandardColaboradorType, 'isMain' | 'description' | 'email' | 'address'>
 
-export const StandardColaborador = (colaborador: StandardColaboradorProps) => {
+export const StandardColaborador = ({ slug, name, className }: StandardColaboradorProps) => {
   return (
-    <div className='w-[300px] h-[150px] relative'>
+    <div className={cn(className, 'w-[300px] h-[150px] relative')}>
       <Image
         fill
         sizes='(max-width: 768px) 100vw, 33vw'
         style={{ objectFit: 'contain' }}
-        src={`/colaboradores/${colaborador.slug}.png`}
-        alt={colaborador.name}
+        src={`/colaboradores/${slug}.png`}
+        alt={name}
         className='max-w-[300px] max-h-[150px] grayscale' // TODO: Remove grayscale when real logos are onboarded?
       />
     </div>
@@ -32,28 +32,26 @@ type MainColaboradorProps = ColaboradorBaseProps & Omit<MainColaboradorType, 'is
 
 export const MainColaborador = ({ className, ...colaborador }: MainColaboradorProps) => {
   return (
-    <div className={cn('flex flex-col gap-3', className)}>
-      <StandardColaborador {...colaborador} />
-      <p className='text-xs text-center text-darkGray'>{colaborador.description}</p>
-      <div className='flex flex-col gap-1'>
-        <p className='text-xs text-center font-semibold text-darkGray'>{colaborador.address}</p>
-        <div className='text-center flex justify-center gap-2'>
-          <a
-            className='text-xs text-center font-semibold text-blue hover:underline'
-            href={`mailto:${colaborador.email}`}
-            target='_blank'
-          >
-            {colaborador.email}
-          </a>
-          <Separator orientation='vertical' className='w-[1px] bg-darkGray' />
-          <a
-            className='text-xs text-center font-semibold text-blue hover:underline'
-            href={colaborador.website}
-            target='_blank'
-          >
-            {removeHttps(colaborador.website)}
-          </a>
-        </div>
+    <div className={className}>
+      <StandardColaborador {...colaborador} className='mb-3' />
+      <p className='text-xs text-center text-darkGray mb-3'>{colaborador.description}</p>
+      <p className='text-xs text-center font-semibold text-darkGray mb-1'>{colaborador.address}</p>
+      <div className='text-center flex justify-center gap-2'>
+        <a
+          className='text-xs text-center font-semibold text-blue hover:underline'
+          href={`mailto:${colaborador.email}`}
+          target='_blank'
+        >
+          {colaborador.email}
+        </a>
+        <Separator orientation='vertical' className='w-[1px] bg-darkGray' />
+        <a
+          className='text-xs text-center font-semibold text-blue hover:underline'
+          href={colaborador.website}
+          target='_blank'
+        >
+          {removeHttps(colaborador.website)}
+        </a>
       </div>
     </div>
   )
