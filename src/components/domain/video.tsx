3c18e3b6e186dd89a5126972c019b0789413d863
Facundo Marcó del Pont <facundo.marcodelpont@replypro.io>
2024-05-16 20:53:16 -0300

    video component

--- src/components/domain/video.tsx
+++ src/components/domain/video.tsx
@@ -19,10 +19,11 @@ export const Video = ({ src, title, href }: VideoProps) => {
       />
       <div className='absolute inset-0 bg-black opacity-50 z-20' />
       <div className='absolute inset-0 flex flex-col z-30'>
-        <h2 className='text-white text-6xl uppercase m-auto'>{title}</h2>
+        <h2 className='text-white text-6xl uppercase m-auto cursor-default'>{title}</h2>
         {href && (
+          // TODO: Add the link to the whole component?
           <Link href={href}>
-            <ChevronRight className='absolute bottom-2 right-2 p-4 rounded-full' size='76px' color='white' />
+            <ChevronRight className='absolute bottom-2 right-2 p-4 rounded-full' size='76px' color='white' />{' '}
           </Link>
         )}
       </div>
