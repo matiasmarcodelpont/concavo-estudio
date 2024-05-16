7dc21ebe651479cd74e67232240ce345496550d6
Facundo Marcó del Pont <facundo.marcodelpont@replypro.io>
2024-05-16 20:49:06 -0300

    video component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { productosRepository } from '@/controllers'
 import { Producto } from '@/components/domain/producto'
 import { GridFluid } from '@/components/layouts/fluid'
+import { Video } from '@/components/domain/video'
 
 export default function Home() {
   const productos = productosRepository.getProductosConcavo()
@@ -10,6 +11,12 @@ export default function Home() {
       <h1>Cóncavo</h1>
 
       <section className='mx-12'>
+        <Video
+          src='https://player.vimeo.com/video/941087021?muted=1&amp;autoplay=1&amp;loop=1&amp;background=1&amp;app_id=122963'
+          title='Casa Cóncavo'
+          href='/casa'
+        />
+
         <h1 className='text-center' id='productos-concavo'>
           Productos Cóncavo
         </h1>
---
+++ src/components/domain/video.spec.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+
+import { Video } from './video'
+
+describe('Video component', () => {
+  it('renders an iframe with the correct src', () => {
+    const src = 'https://www.example.com/video'
+
+    const component = render(<Video src={src} title='cuchuflito' />)
+
+    const iframe = component.getByTitle('cuchuflito')
+    expect(iframe).toHaveAttribute('allow', 'autoplay; fullscreen; picture-in-picture; clipboard-write')
+  })
+
+  it('renders a link if a href prop is passed', () => {
+    const src = 'https://www.example.com/video'
+    const href = 'fake-link'
+
+    const component = render(<Video src={src} title='cuchuflito' href={href} />)
+
+    const link = component.getByRole('link')
+    expect(link).toHaveAttribute('href', href)
+  })
+})
