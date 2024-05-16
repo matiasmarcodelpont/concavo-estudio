59600cf799a53edbba1e224849fadcab90e1eb5d
Facundo Marcó del Pont <facundo.marcodelpont@replypro.io>
2024-05-16 20:56:34 -0300

    video component

--- src/components/domain/video.spec.tsx
+++ src/components/domain/video.spec.tsx
@@ -4,6 +4,11 @@ import { render } from '@testing-library/react'
 import { Video } from './video'
 
 describe('Video component', () => {
+  it('matches snapshot', () => {
+    const { container } = render(<Video src='https://www.example.com/video' title='cuchuflito' href='fake-link' />)
+    expect(container).toMatchSnapshot()
+  })
+
   it('renders an iframe with the correct src', () => {
     const src = 'https://www.example.com/video'
 
