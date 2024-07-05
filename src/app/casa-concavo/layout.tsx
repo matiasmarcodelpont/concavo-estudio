e981927d40b940dcf081c3ad879e86f4d8fb9d6e
Matías Marcó del Pont <matiasmdelp@gmail.com>
2024-07-05 19:29:51 -0300

    Refactor icons

--- src/app/casa-concavo/layout.tsx
+++ src/app/casa-concavo/layout.tsx
@@ -3,6 +3,10 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Casa Cóncavo',
   description: 'Una serie de casas co-creadas por Estudio Cóncavo.',
+  icons: {
+    icon: '/manifest/favicon_casa.ico',
+    apple: '/manifest/apple_icon_casa.png',
+  },
 }
 
 export default function RootLayout({
