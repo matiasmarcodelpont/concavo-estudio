267b87e6f436f6dceea7c554c9d161da377230ed
Matías Marcó del Pont <matiasmdelp@gmail.com>
2024-07-20 00:34:35 -0300

    Ajuste color de fondo y footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -43,11 +43,11 @@ export const Footer = () => {
   ]
 
   return (
-    <footer className='bg-black text-bone flex gap-10 sm:gap-14 px-6 sm:px-10 md:px-12 pt-10 pb-6 flex-wrap'>
+    <footer className='bg-white text-black flex gap-10 sm:gap-14 px-6 sm:px-10 md:px-12 pt-10 pb-6 flex-wrap'>
       {siteMap.map(({ name, links }, index) => (
         <section key={`sitemap-${index.toString()}-${name}`}>
-          <h1 className='font-tt-norms uppercase text-xl mb-2'>{name}</h1>
-          <ul className='text-xs gap-y-2 gap-x-6 flex flex-col flex-wrap max-h-[200px]'>
+          <h1 className='font-tt-norms font-bold uppercase mb-2'>{name}</h1>
+          <ul className='text-xs gap-y-2 gap-x-6 flex flex-col flex-wrap max-h-[150px]'>
             {links.map(({ name, slug, href }) => (
               <li key={slug} className='hover:underline'>
                 <a href={href}>{name}</a>
@@ -58,7 +58,7 @@ export const Footer = () => {
       ))}
 
       <section className='flex gap-6 sm:gap-10 ml-auto'>
-        <LogoAndSocialMedia />
+        <LogoAndSocialMedia variant='black' />
         <Qr value={currentUrl} onClick={handleClick} />
         <div className='absolute left-[-100vw]'>
           <Qr ref={qrRef} value={currentUrl} size={300} />
--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -14,7 +14,7 @@ export const Header = ({ ambientes }: HeaderProps) => {
       <Link href='/' className='uppercase text-lg text-center flex flex-col items-center m-auto p-4 md:p-6'>
         <Logo />
       </Link>
-      <div className='flex-shrink-0 w-[40px] h-[10px] bg-mustard sm:size-9 invisible' />
+      <div className='flex-shrink-0 w-[40px] h-[10px] sm:size-9 invisible' />
     </header>
   )
 }
