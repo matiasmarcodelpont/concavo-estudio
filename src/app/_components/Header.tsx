83dae0a32c8d440968a9dd7a7cea84487f99c984
Matías Marcó del Pont <matiasmdelp@gmail.com>
2024-07-21 13:54:34 -0300

    Adjust header

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,20 +1,29 @@
+'use client'
+
 import Logo from '@/components/domain/Logo'
 import Link from 'next/link'
 import { NavBarLayout } from './NavBarLayout'
 import { Ambiente, OmitReferences } from '@/data/types'
+import { usePathname } from 'next/navigation'
 
 interface HeaderProps {
   ambientes: OmitReferences<Omit<Ambiente, 'images'>>[]
 }
 
 export const Header = ({ ambientes }: HeaderProps) => {
+  const pathname = usePathname()
+
   return (
-    <header className='px-4 flex items-center gap-2 w-full z-40 bg-bone shadow animate-navbar-fade-in opacity-90 sticky top-0'>
+    <header className='fixed top-0 w-full bg-bone z-10 flex justify-between px-2 sm:px-6 py-4 items-center'>
       <NavBarLayout ambientes={ambientes} />
-      <Link href='/' className='uppercase text-lg text-center flex flex-col items-center m-auto p-4 md:p-6'>
-        <Logo />
-      </Link>
-      <div className='flex-shrink-0 w-[40px] h-[10px] sm:size-9 invisible' />
+
+      <h1 className={pathname == '/' ? 'invisible' : ''}>
+        <Link href='/'>
+          <Logo />
+        </Link>
+      </h1>
+
+      <div className='w-[40px]' />
     </header>
   )
 }
