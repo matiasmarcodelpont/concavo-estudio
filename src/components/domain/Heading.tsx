2b79bfbcf36d9ec1baeb0abac59efb0c9520a7bb
Matías Marcó del Pont <48724650+matiasmarcodelpont@users.noreply.github.com>
2024-11-19 19:32:08 -0300

    Refactor about us page (#79)

--- src/components/domain/Heading.tsx
+++ src/components/domain/Heading.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { cn } from '@/lib/utils'
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 import { Waypoint } from 'react-waypoint'
 
 interface HeadingProps {
-  children: string
+  children: ReactNode
   animated?: boolean
   secondary?: boolean
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p'
@@ -30,8 +30,8 @@ const Heading = ({
       <Component
         {...headingProps}
         className={cn(
-          'font-editorial-new tracking-[0.01em] uppercase mt-12 mb-6 sm:mt-16 sm:mb-9 md:mt-20 md:mb-12',
-          secondary ? 'text-2xl md:text-3xl' : 'text-3xl md:text-4xl',
+          'font-editorial-new tracking-[0.01em] font-light uppercase mt-12 mb-6 sm:mt-16 sm:mb-9 md:mt-20 md:mb-12',
+          secondary ? 'text-3xl md:text-4xl' : 'text-4xl md:text-5xl',
           animated ? (animate ? 'animate-slide-up' : 'opacity-0') : 'opacity-100',
           headingProps.className,
         )}
